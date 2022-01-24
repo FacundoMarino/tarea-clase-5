8c128aa3967f03eea8ddb55148e21d41a33c0d28
FacundoMarino <38021147+FacundoMarino@users.noreply.github.com>
2022-01-24 15:40:18 -0300

    corrijo ejercicio 3 de clase 5
    Se cambia la estructura del JS y html

--- tarea-3.js
+++ tarea-3.js
@@ -6,42 +6,53 @@
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
 
+document.querySelector('#calcular').onclick = function(){
 
+    let horaVideo1 = Number(document.querySelector('#hora').value)
+    let horaVideo2 = Number(document.querySelector('#hora2').value)
+    let horaVideo3 = Number(document.querySelector('#hora3').value)
+    let horaVideo4 = Number(document.querySelector('#hora4').value)
+    let horaVideo5 = Number(document.querySelector('#hora5').value)
 
-document.querySelector('#enviar1').onclick = function(){
-    const horaVideoUno = Number(document.querySelector('#hora1').value);
-    const minutosVideoUno = Number(document.querySelector('#minutos1').value);
-    const segundosVideoUno = Number(document.querySelector('#segundos1').value);
+let horasTotal = horaVideo1 + horaVideo2 + horaVideo3 + horaVideo4 + horaVideo5
     
-    return horaVideoUno, minutosVideoUno, segundosVideoUno;
-    return false;
+    let minutosVideo1 = Number(document.querySelector('#minutos').value)
+    let minutosVideo2 = Number(document.querySelector('#minutos2').value)
+    let minutosVideo3 = Number(document.querySelector('#minutos3').value)
+    let minutosVideo4 = Number(document.querySelector('#minutos4').value)
+    let minutosVideo5= Number(document.querySelector('#minutos5').value)
 
+let minutosTotal = minutosVideo1 + minutosVideo2 + minutosVideo3 + minutosVideo4 + minutosVideo5
+
+    let segundosVideo1 = Number(document.querySelector('#segundos').value)
+    let segundosVideo2 = Number(document.querySelector('#segundos2').value)
+    let segundosVideo3 = Number(document.querySelector('#segundos3').value)
+    let segundosVideo4 = Number(document.querySelector('#segundos4').value)
+    let segundosVideo5 = Number(document.querySelector('#segundos5').value)
+
+let segundosTotal = segundosVideo1 + segundosVideo2 + segundosVideo3 + segundosVideo4 + segundosVideo5
+
+
+while(segundosTotal > 60){
+        minutosTotal++
+        segundosTotal  -= 60 
+        
+    }
+
+    while(minutosTotal > 60){
+        horasTotal++
+        minutosTotal  -= 60 
+        
+    }
+
+let resultado = `${horasTotal}:${minutosTotal}:${segundosTotal}`;
+
+let mostrarResultado = document.querySelector('#resultado')
+
+mostrarResultado.textContent = resultado;
+
+
+console.log(horasTotal)
+console.log(minutosTotal)
+console.log(segundosTotal)
 }
-
-document.querySelector('#enviar2').onclick = function(){
-    const horaVideoDos = Number(document.querySelector('#hora2').value);
-    const minutosVideoDos = Number(document.querySelector('#minutos2').value);
-    const segundosVideoDos = Number(document.querySelector('#segundos2').value);
-    
-    return false;
-
-}
-
-document.querySelector('#enviar3').onclick = function(){
-    const horaVideoTres = Number(document.querySelector('#hora3').value);
-    const minutosVideoTres = Number(document.querySelector('#minutos3').value);
-    const segundosVideoTres = Number(document.querySelector('#segundos3').value);
-    
-    return false;
-
-}
-
-let totalHoras = horaVideoUno + horaVideoDos + horaVideoTres;
-let totalMinutos = minutosVideoUno + minutosVideoDos + minutosVideoTres;
-let totalSegundos = segundosVideoUno + segundosVideoDos + segundosVideoTres;
-
-console.log(totalHoras);
-
-console.log(totalMinutos);
-
-console.log(totalSegundos);
